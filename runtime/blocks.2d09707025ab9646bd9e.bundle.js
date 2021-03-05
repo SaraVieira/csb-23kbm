12971f741daf6d5fef6079df8eb9f6442dc80932
Code Sandbox <hello@codesandbox.io>
2021-03-05 16:27:38 +0000

    Deployment from Codesandbox.

---
+++ runtime/blocks.2d09707025ab9646bd9e.bundle.js
@@ -0,0 +1,11 @@
+/*!
+ * 
+ * [Dojo](https://dojo.io/)
+ * Copyright [JS Foundation](https://js.foundation/) & contributors
+ * [New BSD license](https://github.com/dojo/meta/blob/master/LICENSE)
+ * All rights reserved
+ * 
+ */
+(window.dojoWebpackJsonpdojo_codesandbox_template=window.dojoWebpackJsonpdojo_codesandbox_template||[]).push([["runtime/blocks"],{12:function(o,d,n){window.blockCacheEntry=function(o,d,e){window.__dojoBuildBridgeCache=window.__dojoBuildBridgeCache||{},window.__dojoBuildBridgeCache[o]=window.__dojoBuildBridgeCache[o]||{},window.__dojoBuildBridgeCache[o][d]=function(){return n.e(e).then(n.bind(null,e+".js")).then(function(o){return o.default})}};
+/** @preserve APPEND_BLOCK_CACHE_ENTRY **/}}]);
+//# sourceMappingURL=blocks.2d09707025ab9646bd9e.bundle.js.map
